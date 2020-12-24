5f246df1a7890515450167786f711b0e61bb2928
islamelhady <i.elhady44@gmail.com>
2020-12-24 05:11:43 +0200

    commit

---
+++ hackerrank/Java Stdin and Stdout I.java
@@ -0,0 +1,20 @@
+// Created by islam elhady on 25/01/2018.
+
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       Scanner scan = new Scanner(System.in);
+        int a , b, c;
+         a = scan.nextInt();
+         b = scan.nextInt();
+         c = scan.nextInt();
+        // Complete this line
+        // Complete this line
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}

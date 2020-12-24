91130dae14dc04807ec5a22b5a0dc7d1956a9b0c
islamelhady <i.elhady44@gmail.com>
2020-12-24 05:14:59 +0200

    commit

---
+++ hackerrank/Welcome to Java!.java
@@ -0,0 +1,10 @@
+// Created by islam elhady on 25/01/2018.
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
+        System.out.println("Hello, World.\n"+"Hello, Java.\n");
+        
+    }
+}

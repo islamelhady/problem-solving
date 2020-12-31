8c503b4213d86d0ff77d35dee1265c29c6986b59
islamelhady <i.elhady44@gmail.com>
2020-12-31 07:39:38 +0200

    data and time

---
+++ hackerrank/Java Date and Time.java
@@ -0,0 +1,16 @@
+// Created by islam elhady on 02/02/2019.
+
+import java.time.LocalDate;
+
+class Result {
+
+
+    public static String findDay(int month, int day, int year) {
+        int d = Integer.valueOf(day);
+        int m = Integer.valueOf(month);
+        int y = Integer.valueOf(year);
+        LocalDate date = LocalDate.of(y, m, d);
+        return date.getDayOfWeek().toString();
+    }
+
+}

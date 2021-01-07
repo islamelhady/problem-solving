fce46c2bf895c0a88e32eac93f7cbd0c24a06f6c
islamelhady <i.elhady44@gmail.com>
2021-01-07 07:45:18 +0200

    submissions

---
+++ hackerrank/Java Substring.java
@@ -0,0 +1,19 @@
+// Created by islam elhady on 09/05/2019.
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+    public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.next();
+        int start  = scan.nextInt();
+        int end    = scan.nextInt();
+        scan.close();
+        
+        System.out.println(str.substring(start, end));
+    }
+}

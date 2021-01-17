9e9bf81a313135044c2c09536f4facb255da6a10
islamelhady <i.elhady44@gmail.com>
2021-01-17 08:04:55 +0200

    solve simple product

---
+++ urionlinejudge/1004_Simple_Product.java
@@ -0,0 +1,20 @@
+// Created by islam elhady on 20/01/2018.
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+       
+       int A,B,PROD;
+       Scanner s = new Scanner (System.in);
+       A = s.nextInt();
+       B = s.nextInt();
+       PROD = A*B;
+       System.out.println("PROD = " + PROD);
+ 
+    }
+ 
+}

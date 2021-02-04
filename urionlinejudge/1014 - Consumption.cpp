860f242889dce4bf4bb4536c4d973ceb41ee82ed
islamelhady <i.elhady44@gmail.com>
2021-02-04 09:37:41 +0200

    consumption

---
+++ urionlinejudge/1014 - Consumption.cpp
@@ -0,0 +1,15 @@
+// Created by islam elhady on 9/5/20.
+
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+  double X, Y;
+  scanf("%lf %lf", &X, &Y);
+
+  printf("%.3lf km/l\n", X/Y);
+
+  return 0;
+}

6e4a63623d263f3c147b4efbb7d8ee752f450b66
islamelhady <i.elhady44@gmail.com>
2021-02-05 18:44:31 +0200

    distance

---
+++ urionlinejudge/1016 - Distance.java
@@ -0,0 +1,18 @@
+// Created by islam elhady on 1/21/18.
+
+import java.io.IOException;
+import java.util.Scanner;
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner s = new Scanner (System.in);              
+             
+         int x;
+          x = s.nextInt();
+         
+          System.out.println( 2*x + " minutos" );
+ 
+    }
+ 
+}
